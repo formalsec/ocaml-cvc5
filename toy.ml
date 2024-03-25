d9b87aa03b8d9732991d169cc502e09abbffbe68
joaomhmpereira <joaompereira07@gmail.com>
2024-03-25 20:42:34 +0000

    create API

--- toy.ml
+++ toy.ml
@@ -1,28 +1,24 @@
-open Cvc5_cxx.Cvc5_external
-open Cvc5_cxx.Kind
+open Cvc5
 
-let tm = new_term_manager ()
-let solver = new_solver tm
-let _true_term = mk_true tm
-let _false_term = mk_false tm
-let _bool_sort = get_boolean_sort tm
-let _int_sort = get_integer_sort tm
-let x = mk_const tm _int_sort "x"
-let zero = mk_int tm 0
+let tm = TermManager.mk_tm ()
+let solver = Solver.mk_solver tm
+let int_sort = Sort.mk_int_sort tm
+let x = Term.mk_const_s tm int_sort "x"
+let zero = Term.mk_int tm 0
 
 let () =
-  let x_gt_zero = mk_term tm (to_cpp Gt) [| x; zero |] in
-  print_endline (term_to_string x_gt_zero);
-  let x_lt_zero = mk_term tm (to_cpp Lt) [| x; zero |] in
-  print_endline (term_to_string x_lt_zero);
+  let x_gt_zero = Term.mk_term tm Kind.Gt [|x; zero|] in
+  print_endline (Term.to_string x_gt_zero);
+  let x_lt_zero = Term.mk_term tm Kind.Lt [|zero; x|] in
+  print_endline (Term.to_string x_lt_zero);
   (* x > 0 *)
-  assert_formula solver x_gt_zero;
+  Solver.assert_formula solver x_gt_zero;
   (* x < 0 *)
-  assert_formula solver x_lt_zero;
-  let r = check_sat solver in
-  let b = result_is_sat r in
-  delete solver;
-  delete_term_manager tm;
+  Solver.assert_formula solver x_lt_zero;
+  let r = Solver.check_sat solver in
+  let b = Result.is_sat r in
+  Solver.delete_solver solver;
+  TermManager.delete_tm tm;
   match b with
   | true -> print_endline "sat OCaml"
-  | false -> print_endline "unsat OCaml"
+  | false -> print_endline "unsat OCaml" 
