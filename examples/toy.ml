d87605215ef85676c057c379440f7a7338f3cd76
joaomhmpereira <joaompereira07@gmail.com>
2024-04-07 00:09:47 +0100

    add comments to toy example

--- examples/toy.ml
+++ examples/toy.ml
@@ -1,26 +1,37 @@
 open Cvc5
 
-let tm = TermManager.mk_tm ()
-
-let solver = Solver.mk_solver tm
-
-let int_sort = Sort.mk_int_sort tm
-
-let x = Term.mk_const_s tm int_sort "x"
-
-let zero = Term.mk_int tm 0
-
+(* This example demonstrates how to create a simple formula and check its
+   satisfiability. *)
 let () =
-  (* x > 0 *)
+  (* Create a TermManager *)
+  let tm = TermManager.mk_tm () in
+  (* Create a Solver *)
+  let solver = Solver.mk_solver tm in
+  (* Create an integer sort *)
+  let int_sort = Sort.mk_int_sort tm in
+  (* Create an integer constant term 'x' *)
+  let x = Term.mk_const_s tm int_sort "x" in
+  (* Create an integer term with value 0 *)
+  let zero = Term.mk_int tm 0 in
+
+  (* Create the term: x >= 0 *)
   let x_gt_zero = Term.mk_term tm Kind.Geq [| x; zero |] in
-  Printf.printf "term 1: %s\n" (Term.to_string x_gt_zero);
-  (* x <= 0 *)
+  (* Create the term: x <= 0 *)
   let x_lt_zero = Term.mk_term tm Kind.Leq [| x; zero |] in
-  Printf.printf "term 2: %s\n" (Term.to_string x_lt_zero);
-  Solver.assert_formula solver x_gt_zero;
-  Solver.assert_formula solver x_lt_zero;
-  let r = Solver.check_sat solver in
-  let b = Result.is_sat r in
-  Solver.delete_solver solver;
+  (* Create a conjunction of both terms *)
+  let formula = Term.mk_term tm Kind.And [| x_gt_zero; x_lt_zero |] in
+  (* Print the created formula *)
+  Printf.printf "Formula: %s\n" (Term.to_string formula);
+
+  (* Assert the formula *)
+  Solver.assert_formula solver formula;
+
+  (* Obtain the satisfiability result of the asserted formulas *)
+  let result = Solver.check_sat solver in
+  ( match Result.is_sat result with
+  | true -> Printf.printf "sat\n"
+  | false -> Printf.printf "unsat\n" );
+
+  (* Delete the TermManager and the Solver *)
   TermManager.delete_tm tm;
-  match b with true -> print_endline "sat" | false -> print_endline "unsat"
+  Solver.delete_solver solver
