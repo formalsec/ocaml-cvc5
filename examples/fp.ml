4cde7e9e5bcb2893586bc23806c1a3b39967d50c
joaomhmpereira <joaompereira07@gmail.com>
2024-05-10 01:09:48 +0100

    add example for floating point arithmetic

---
+++ examples/fp.ml
@@ -0,0 +1,82 @@
+(*------------------------------------------------------------------------*)
+(*  Copyright (C) 2024 formalsec                                          *)
+(*                                                                        *)
+(*  This file is part of ocaml-cvc5                                       *)
+(*                                                                        *)
+(*  ocaml-cvc5 is free software: you can redistribute it and/or modify    *)
+(*  it under the terms of the GNU General Public License as published     *)
+(*  by the Free Software Foundation, either version 3 of the License,     *)
+(*  or (at your option) any later version.                                *)
+(*                                                                        *)
+(*  ocaml-cvc5 is distributed in the hope that it will be useful,         *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *)
+(*  See the GNU General Public License for more details.                  *)
+(*                                                                        *)
+(*  You should have received a copy of the GNU General Public License     *)
+(*  along with ocaml-cvc5. If not, see <http://www.gnu.org/licenses/>.    *)
+(*------------------------------------------------------------------------*)
+
+(* Example taken from:
+   https://github.com/florianschanda/smtlib_schanda/blob/master/crafted/QF_FP/assoc_1.smt2 *)
+
+open Cvc5
+
+let tm = TermManager.mk_tm ()
+
+let solver = Solver.mk_solver ~logic:"QF_FP" tm
+
+let fp_sort = Sort.mk_fp_sort tm 8 24
+
+let rm_sort = Sort.mk_rm_sort tm
+
+let x = Term.mk_const_s tm fp_sort "x"
+
+let y = Term.mk_const_s tm fp_sort "y"
+
+let z = Term.mk_const_s tm fp_sort "z"
+
+let rm = Term.mk_const_s tm rm_sort "rm"
+
+let () =
+  let bv_one = Term.mk_bv_s tm 16 "1" 10 in
+  let bv_zero = Term.mk_bv_s tm 16 "0" 10 in
+  
+  let fp_one = Term.mk_fp tm 8 24 bv_one in
+  let fp_zero = Term.mk_fp tm 8 24 bv_zero in
+
+  let rne = Term.mk_rm tm RoundingMode.Rne in
+
+  let to_fp = Op.mk_op tm Kind.Floatingpoint_to_fp_from_fp [| 8; 24 |] in
+
+  let t1 = Term.mk_term tm Kind.Floatingpoint_eq [|
+    Term.mk_term tm Kind.Floatingpoint_add [|
+      rm; x;
+      Term.mk_term tm Kind.Floatingpoint_add [|
+        rm; y; z
+      |]
+    |];
+    Term.mk_term_op tm to_fp [| rne; fp_zero |]
+  |] in
+
+
+  let t2 = Term.mk_term tm Kind.Floatingpoint_eq [|
+    Term.mk_term tm Kind.Floatingpoint_add [|
+      rm;
+      Term.mk_term tm Kind.Floatingpoint_add [|
+        rm; x; y
+      |]; z
+    |];
+    Term.mk_term_op tm to_fp [| rne; fp_one |]
+  |] in
+  
+  Printf.printf "t1: %s\n" (Term.to_string t1);
+  Printf.printf "t2: %s\n" (Term.to_string t2);
+
+  Solver.assert_formula solver t1;
+  Solver.assert_formula solver t2;
+  let r = Solver.check_sat solver in
+
+  match Result.is_sat r with
+  | true -> Printf.printf "sat\n"
+  | false -> Printf.printf "unsat\n"
---
+++ examples/fp.smt2
@@ -0,0 +1,21 @@
+(set-info :smt-lib-version 2.6)
+(set-info :license |https://www.gnu.org/licenses/gpl-3.0.html|)
+(set-logic QF_FP)
+(set-info :source |SPARK inspired floating point problems by Florian Schanda|)
+(set-info :category crafted)
+(set-info :status sat)
+
+(declare-const x Float32)
+(declare-const y Float32)
+(declare-const z Float32)
+(declare-const rm RoundingMode)
+
+;; x + y + z can be 0 or 1 depending on order of evaluation
+
+(assert (fp.eq (fp.add rm x (fp.add rm y z))
+               ((_ to_fp 8 24) RNE 0)))
+(assert (fp.eq (fp.add rm (fp.add rm x y) z)
+               ((_ to_fp 8 24) RNE 1)))
+
+(check-sat)
+(exit)
