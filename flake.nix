901fd076fc6fc9f8e1a5bebdbabb71e34c1bd85c
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-06-24 09:14:31 +0100

    Add flake.nix for opam build

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "OCaml development environment";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Define supported systems
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      # Helper function to generate attributes for each system
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          pythonWithPkgs = pkgs.python313.withPackages (ps: with ps; [
+            pyparsing
+            tomli
+          ]);
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              opam
+              pkg-config
+              pythonWithPkgs
+            ];
+
+            buildInputs = with pkgs; [
+              zlib
+              gmp.dev
+              gmpxx
+              m4
+              flint
+              cmake
+            ];
+
+            shellHook = ''
+              export PATH="${pythonWithPkgs}/bin:$PATH"
+              export NIX_PYTHONPATH="${pythonWithPkgs}/lib/python3.13/site-packages"
+            '';
+          };
+        }
+      );
+    };
+}
